08276cec3343845bd2fe3b97a50b2b31adce4d12
kazhurka <42044750+kazhurka@users.noreply.github.com>
2020-04-17 14:12:29 +0300

    Устраняет баги

--- build/js/login.js
+++ build/js/login.js
@@ -1,18 +1,52 @@
 'use strict';
 (function () {
-  var loginModal = document.querySelector('.login');
-  var loginOpen = document.querySelector('.main-header__user-link--login');
-  var loginClose = loginModal.querySelector('.login__close');
+  var ESCAPE = 'Escape';
+  var MAINBUTTON = '0';
+  var modalLogin = document.querySelector('.login');
+  var addButton = document.querySelector('.main-header__user-link--login');
+  var closeButton = modalLogin.querySelector('.login__close');
   var modalWrap = document.querySelector('.modal-wrap');
+  var body = document.querySelector('body');
 
-  loginOpen.addEventListener('click', function () {
-    loginModal.classList.add('login--active');
-    modalWrap.classList.add('modal-wrap--active');
-  });
-
-  loginClose.addEventListener('click', function () {
-    loginModal.classList.remove('login--active');
+  var closeModal = function () {
+    console.log("ss")
     modalWrap.classList.remove('modal-wrap--active');
-  });
+    modalLogin.classList.remove('login--active');
+    body.classList.remove('body--modal')
+  }
+  var removeModalHandlers = function () {
+    document.removeEventListener('keydown', modalKeyCloseHandler);
+    closeButton.removeEventListener('click', modalMouseCloseHandler);
+    document.removeEventListener('click', modalOverClickHandler);
+  }
+  var modalKeyCloseHandler = function (evtEsc) {
+    if (evtEsc.key === ESCAPE) {
+      closeModal();
+      removeModalHandlers();
+    }
+  }
 
+  var modalOverClickHandler = function (evtOver) {
+    if (evtOver.target === modalWrap || evtOver.target === closeButton) {
+      closeModal();
+      removeModalHandlers();
+    }
+  }
+  var modalMouseCloseHandler = function (evtM) {
+    if (evtM.button === MAINBUTTON) {
+      closeModal();
+      removeModalHandlers()
+    }
+  }
+
+  var modalOpenHandler = function () {
+    modalWrap.classList.add('modal-wrap--active');
+    modalLogin.classList.add('login--active');
+    body.classList.add('body--modal')
+    closeButton.addEventListener('click', modalMouseCloseHandler);
+    modalWrap.addEventListener('click', modalOverClickHandler)
+    document.addEventListener('keydown', modalKeyCloseHandler)
+  }
+
+  addButton.addEventListener('click', modalOpenHandler);
 })();
